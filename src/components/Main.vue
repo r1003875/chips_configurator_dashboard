b1712d6a0abd6f70c899b9bcd9c8cfcda1d1957c
Simon Van den Eynde <r1003875@student.thomasmore.be>
2026-01-03 15:08:55 +0100

    feat: enhance submissions display and improve layout in Main and Submission components

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,21 +1,29 @@
 <script setup>
     import Submission from './Submission.vue';
+    import {onMounted, reactive, ref} from 'vue';
+
+    let submissions = reactive([]);
+
+    onMounted(async() => {
+    // Fetch initial chat messages or perform setup
+    const response = await fetch('http://localhost:3000/api/v1/bags');
+    const data = await response.json();
+    console.log(data.data.bags);
+    submissions.push(...data.data.bags);
+    submissions.reverse();
+    console.log(submissions);
+    });
 </script>
 
 <template>
     <h2>Submissions</h2>
     <div class="submissions">
         <!-- Submission items will go here -->
-        <Submission />
-        <Submission />
-        <Submission />
-        <Submission />
-        <Submission />
-        <Submission />
-        <Submission />
-        <Submission />
-        <Submission />
-        <Submission />
+        <Submission 
+            v-for="submission in submissions"
+            :key="submission.id"
+            :submission="submission"
+        />
     </div>
 </template>
 
--- src/components/Submission.vue
+++ src/components/Submission.vue
@@ -1,11 +1,25 @@
 <script setup>
+    import { defineProps } from 'vue';
 
+    const props = defineProps({
+        submission: {
+            type: Object,
+            required: true
+        }
+    });
 </script>
 
 <template>
     <div class="submission">
-        <h3>Submission Title</h3>
-        <p>Details about the submission...</p>
+        <div>
+            <h3>{{ submission.name }}</h3>
+            <h4>Details:</h4>
+            <p>Image: {{ submission.image }}</p>
+            <p>Color: {{ submission.color }}</p>
+            <p>Font: {{ submission.font }}</p>
+            <p>Image_URL: {{ submission.image }}</p>
+            <p>Flavours: {{ submission.keyFlavours }}</p>
+        </div>
         <div class="remove_btn">Remove</div>
     </div>
 </template>
