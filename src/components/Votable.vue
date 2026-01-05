00ffcbee0422ca58553618c544c291cf7f79052b
Simon Van den Eynde <r1003875@student.thomasmore.be>
2026-01-05 16:22:02 +0100

    feat: add Votable component for submission voting and implement voting functionality

---
+++ src/components/Votable.vue
@@ -0,0 +1,34 @@
+<script setup>
+    import { defineProps } from 'vue';
+
+    const props = defineProps({
+        submission: {
+            type: Object,
+            required: true
+        }
+    });
+
+    const emit = defineEmits(['vote']);
+</script>
+
+<template>
+    <div class="submission">
+        <div>
+            <h3>{{ submission.name }}</h3>
+            <h4>Details:</h4>
+            <div>
+                <p>Image:</p>
+                <img :src="submission.image" alt="chips image" width="200" />
+            </div>
+            <p>Bag Color: <span :style="{ color: submission.color }">{{ submission.color }}</span></p>
+            <p>Font: {{ submission.font }}</p>
+            <p>Flavours: {{ submission.keyFlavours }}</p>
+            <p>User_id: {{ submission.user }}</p>
+        </div>
+        <div @click.prevent="emit('vote', submission._id)" class="vote_btn" title="Vote for this submission?">Vote</div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
--- src/components/Voting.vue
+++ src/components/Voting.vue
@@ -1,10 +1,53 @@
 <script setup>
     import Nav from './Nav.vue';
+    import { onMounted, reactive } from 'vue';
+    import Votable from './Votable.vue';
+
+    let submissions = reactive([]);
+
+    onMounted(async() => {
+        const response = await fetch('http://localhost:3000/api/v1/bags');
+        const data = await response.json();
+        submissions.push(...data.data.bags);
+        submissions.reverse();
+    });
+
+    async function handleVote(bag_id) {
+        try {
+            const response = await fetch(`http://localhost:3000/api/v1/votes`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ user: sessionStorage.getItem('userId'), bag: bag_id })
+            });
+
+            const json = await response.json();
+
+            if (json.status === 'success') {
+                console.log('Vote cast successfully');
+            } else {
+                console.error('Failed to cast vote:', json.message);
+            }
+        }
+        catch(error){
+            console.error('Error during voting:', error);
+        }
+    }
 </script>
 
 <template>
     <Nav />
     <h2>Voting Page</h2>
+    <p>Welcome to the voting page. Here you can cast your vote!</p>
+    <div class="submissions">
+        <Votable 
+            v-for="submission in submissions"
+            :key="submission.id"
+            :submission="submission"
+            @vote="handleVote"
+        />
+    </div>
 </template>
 
 <style scoped>
