9f67acca66ff8e2cdffc947875c652cdfa43423b
Simon Van den Eynde <r1003875@student.thomasmore.be>
2026-01-07 21:10:10 +0100

    feat: add CONFIG_URL to environment files and enhance Nav component with configurator link

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,24 +1,46 @@
 <script setup>
     import { useRouter } from 'vue-router'
+    import { ref } from 'vue';
+    
+    const CONFIG_URL = import.meta.env.VITE_CONFIG_URL;
 
     const router = useRouter()
 
     const logout = () => {
+        sessionStorage.removeItem('token')
+        sessionStorage.removeItem('userId')
 
-    sessionStorage.removeItem('token')
-    sessionStorage.removeItem('userId')
-
-    router.push({ name: 'Login' })
-}
+        router.push({ name: 'Login' })
+    }
+    const token = sessionStorage.getItem('token');
+    const configuratorLink = ref(`${CONFIG_URL}?token=${token}`)
 </script>
   
 <template>
     <nav>
         <h1>Chips Configurator</h1>
-        <div @click="logout" class="btn">Log out ></div>
+        <div class="btn_container">
+            <div class="btn secondary_btn"><a id="configurator-link" :href="configuratorLink">Make your own bag</a></div>
+            <div @click="logout" class="btn">Log out ></div>
+        </div>
     </nav>
 </template>
 
 <style scoped>
-
+    .btn_container{
+        display: flex;
+        gap: 1rem;
+    }
+    .secondary_btn{
+        background-color: #08a155;
+        color: #ffffff;
+    }
+    .secondary_btn:hover{
+        background-color: #0c9658;
+        color: #ffffff;
+    }
+    a{
+        text-decoration: none;
+        color: inherit;
+    }
 </style>
